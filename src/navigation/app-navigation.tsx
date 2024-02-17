f41e733d51ab4149593beacd9974e7ae58ea5ae9
calebjanhunen <calebjanhunen@gmail.com>
2024-02-16 19:45:32 -0500

    Add icons

--- src/navigation/app-navigation.tsx
+++ src/navigation/app-navigation.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import Home from '../screens/home/home';
 import UserSetup from '../screens/user-setup/user-setup';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,12 +14,22 @@ export default function AppNavigation() {
         <Tab.Screen
           name='Home'
           component={Home}
-          options={{ headerShown: false }}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name='home-outline' color={color} size={size} />
+            ),
+          }}
         />
         <Tab.Screen
           name='User Setup'
           component={UserSetup}
-          options={{ headerShown: false }}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name='cog-outline' color={color} size={size} />
+            ),
+          }}
         />
       </Tab.Navigator>
     </NavigationContainer>
