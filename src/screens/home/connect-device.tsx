ee6f493ff60ca7f6c135e3d4aa58484a453563a8
unknown <calebjanhunen@gmail.com>
2024-03-09 15:05:33 -0500

    Change home page

--- src/screens/home/connect-device.tsx
+++ src/screens/home/connect-device.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
-import { Button, PageView, Text } from '../../components';
-import Spacer from '../../components/spacer/spacer';
-import { useBLE } from '../../context/ble-context';
+import { Button, Text } from 'react-native-paper';
+
+import { PageView } from '@/components';
+import Spacer from '@/components/spacer/spacer';
+import { useBLE } from '@/context/ble-context';
 
 export default function ConnectDevice() {
   const { stopConnecting, isConnecting, connectToDevice } = useBLE();
   return (
     <PageView>
       <Spacer size='md' />
-      <Text variant='smallTitle' textAlign='center'>
+      <Text variant='headlineSmall' style={{ textAlign: 'center' }}>
         Disense socks not connected
       </Text>
+      <Spacer size='xs' />
+      <Text variant='bodyLarge' style={{ textAlign: 'center' }}>
+        Ensure the sock is on and connect via Bluetooth
+      </Text>
+
       <Spacer size='md' />
       <Button
-        variant='full'
-        backgroundColor='primary'
-        textColor='white'
+        mode='contained'
         onPress={isConnecting ? stopConnecting : async () => connectToDevice()}
       >
         {isConnecting ? 'Stop Connecting' : 'Connect to Socks'}
