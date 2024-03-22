191c5c2a3bf4429b9a91ab4e58c1ee5382a465a5
unknown <calebjanhunen@gmail.com>
2024-03-21 21:11:50 -0400

    Add spo2 ulcer risk checker and tests

---
+++ __tests__/spo2-ulcer-risk-checker.test.ts
@@ -0,0 +1,26 @@
+import { SPO2Sensor } from '@/interfaces/Sensor';
+import { checkSpo2ForUlcerRisk } from '@/utils/spo2-ulcer-risk-functions/spo2-ulcer-risk-checker';
+
+describe('checkSpo2ForUlcerRisk function', () => {
+  test('should return array of size 1 if 1 sensor is below threshold', () => {
+    const spo2Data: SPO2Sensor[] = [{ id: 1, heartRate: 70, bloodOxygen: 93 }];
+
+    const result = checkSpo2ForUlcerRisk(spo2Data);
+
+    expect(result).toStrictEqual(spo2Data);
+  });
+  test('should return array of size 0 if the sensor is equal to the threshold', () => {
+    const spo2Data: SPO2Sensor[] = [{ id: 1, heartRate: 70, bloodOxygen: 94 }];
+
+    const result = checkSpo2ForUlcerRisk(spo2Data);
+
+    expect(result).toStrictEqual([]);
+  });
+  test('should return array of size 0 if the sensor is greater than the threshold', () => {
+    const spo2Data: SPO2Sensor[] = [{ id: 1, heartRate: 70, bloodOxygen: 99 }];
+
+    const result = checkSpo2ForUlcerRisk(spo2Data);
+
+    expect(result).toStrictEqual([]);
+  });
+});
