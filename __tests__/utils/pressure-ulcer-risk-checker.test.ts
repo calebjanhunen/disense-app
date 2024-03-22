34b6aff7329a3f6e6aa4578469fa2065f629e1a7
unknown <calebjanhunen@gmail.com>
2024-03-21 21:02:33 -0400

    Add checkPressureForUlcer risk function and tests

---
+++ __tests__/utils/pressure-ulcer-risk-checker.test.ts
@@ -0,0 +1,57 @@
+import { FSR } from '@/interfaces/Sensor';
+import * as forceToPressureCalculation from '@/utils/pressure-calculations/force-to-pressure';
+import { checkPressureForUlcerRisk } from '@/utils/pressure-calculations/pressure-ulcer-risk-checker';
+
+describe('checkPressureForUlcerRisk function', () => {
+  test('should return false if average pressure is below threshold', () => {
+    const fsrData: FSR[] = [
+      { id: 1, force: 800 },
+      { id: 2, force: 820 },
+      { id: 3, force: 910 },
+      { id: 4, force: 739 },
+    ];
+    const shoeSize = 5;
+
+    jest
+      .spyOn(forceToPressureCalculation, 'forceToPressureInKiloPascals')
+      .mockReturnValue(140);
+
+    const result = checkPressureForUlcerRisk(fsrData, shoeSize);
+
+    expect(result).toBe(false);
+  });
+  test('should return false if average pressure is equal to threshold', () => {
+    const fsrData: FSR[] = [
+      { id: 1, force: 800 },
+      { id: 2, force: 820 },
+      { id: 3, force: 910 },
+      { id: 4, force: 739 },
+    ];
+    const shoeSize = 5;
+
+    jest
+      .spyOn(forceToPressureCalculation, 'forceToPressureInKiloPascals')
+      .mockReturnValue(143.6);
+
+    const result = checkPressureForUlcerRisk(fsrData, shoeSize);
+
+    expect(result).toBe(false);
+  });
+  test('should return true if average pressure is greater than threshold', () => {
+    const fsrData: FSR[] = [
+      { id: 1, force: 800 },
+      { id: 2, force: 820 },
+      { id: 3, force: 910 },
+      { id: 4, force: 739 },
+    ];
+    const shoeSize = 5;
+
+    jest
+      .spyOn(forceToPressureCalculation, 'forceToPressureInKiloPascals')
+      .mockReturnValue(150);
+
+    const result = checkPressureForUlcerRisk(fsrData, shoeSize);
+
+    expect(result).toBe(true);
+  });
+});
--- src/utils/pressure-calculations/pressure-ulcer-risk-checker.ts
+++ src/utils/pressure-calculations/pressure-ulcer-risk-checker.ts
@@ -1,5 +1,15 @@
 import { FSR } from '@/interfaces/Sensor';
+import { forceToPressureInKiloPascals } from './force-to-pressure';
 
-export function checkPressureForUlcerRisk(fsrData: FSR[]): FSR[] {
-  // const avgPressure
+const pressureThreshold = 143.6; // In kPa
+
+export function checkPressureForUlcerRisk(
+  fsrData: FSR[],
+  shoeSize: number
+): boolean {
+  const avgPressure = forceToPressureInKiloPascals(fsrData, shoeSize);
+  if (avgPressure > pressureThreshold) {
+    return true;
+  }
+  return false;
 }
